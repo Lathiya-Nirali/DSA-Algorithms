aa921175c967f2073a1ae8b0dfb2ff8e1323b10e
Lathiya-Nirali <70018191+Lathiya-Nirali@users.noreply.github.com>
2026-04-21 19:41:48 +0530

    Create ContainsDuplicae.cs

---
+++ ContainsDuplicae.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        
+        HashSet<int> map = new HashSet<int>();
+
+        for(int i = 0; i< nums.Length; i++)
+        {
+            if(map.Contains(nums[i]))
+                return true;
+
+            map.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
