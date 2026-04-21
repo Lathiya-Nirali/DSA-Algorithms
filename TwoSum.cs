3c10a229c868b12a830844d080c7b0761fc72008
Lathiya-Nirali <70018191+Lathiya-Nirali@users.noreply.github.com>
2026-04-21 18:17:37 +0530

    Add files via upload

---
+++ TwoSum.cs
@@ -0,0 +1,28 @@
+﻿namespace DSA_Algos
+{
+    public static class TwoSum
+    {
+        public static int[] TwoSumFunc(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int need = target - nums[i];
+
+                if (map.ContainsKey(need))
+                {
+                    return new int[] { map[need], i };
+                }
+
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map[nums[i]] = i;
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
