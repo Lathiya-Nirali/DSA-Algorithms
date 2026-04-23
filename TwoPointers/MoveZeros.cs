efa59ebc1d92f9fd1208a7c8fce48f495705e6ef
Lathiya-Nirali <70018191+Lathiya-Nirali@users.noreply.github.com>
2026-04-23 19:20:04 +0530

    Create MoveZeros.cs

---
+++ TwoPointers/MoveZeros.cs
@@ -0,0 +1,15 @@
+ public void MoveZeroes(int[] nums) {
+        
+        int left = 0;
+
+        for(int i = 0; i<nums.Length; i++)
+        {
+            if(nums[i] != 0)
+            {
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
+            }
+        }
+    }
